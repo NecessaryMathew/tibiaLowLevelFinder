6ff75a6cbea93a123a8bc8f3bf4b47c357f7d549
Mateusz Konieczny <mateusz.konieczny@hotmail.com>
2024-03-07 23:39:33 +0100

    initial commit

---
+++ src/main/java/com/example/tibialowlevelfinder/TibiaLowLevelFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.tibialowlevelfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibiaLowLevelFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TibiaLowLevelFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tibialowlevelfinder/TibiaLowLevelFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tibialowlevelfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaLowLevelFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
